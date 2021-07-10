c6e92e30c87ee4933eae5f1648b91f498d535352
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-07-10 13:15:51 -0300

    fixed color-picker

--- color-picker.py
+++ color-picker.py
@@ -10,30 +10,35 @@ def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = image_bgr[y, x]
 
-        #you might want to adjust the ranges(+-10, etc):
-        upper =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 40])
-        lower =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 40])
+        # you might want to adjust the ranges(+-10, etc):
+        pixel1 = 255 if pixel[0] + 40 > 255 else pixel[0] + 40
+        pixel2 = 255 if pixel[1] + 40 > 255 else pixel[1] + 40
+        pixel3 = 255 if pixel[2] + 40 > 255 else pixel[2] + 40
+        pixel4 = 0 if pixel[0] - 40 < 0 else pixel[0] - 40
+        pixel5 = 0 if pixel[1] - 40 < 0 else pixel[1] - 40
+        pixel6 = 0 if pixel[2] - 40 < 0 else pixel[2] - 40
+        upper = '[' + str(pixel1) + ', ' + str(pixel2) + ', ' + str(pixel3) + ']'
+        lower = '[' + str(pixel4) + ', ' + str(pixel5) + ', ' + str(pixel6) + ']'
         print(lower, pixel, upper)
 
-        image_mask = cv2.inRange(image_bgr, lower, upper)
+        upperNp =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 40])
+        lowerNp =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 40])
+
+        image_mask = cv2.inRange(image_bgr, lowerNp, upperNp)
         cv2.imshow("mask", image_mask)
 
 def main():
     global image_bgr, pixel # so we can use it in mouse callback
 
-    image_src = cv2.imread(sys.argv[1])  # pick.py my.png
-    if image_src is None:
+    image_bgr = cv2.imread(sys.argv[1])  # pick.py my.png
+    if image_bgr is None:
         print ("Use python3.7 color-pycker.py path/to/image")
         return
-    cv2.imshow("bgr", image_src)
+    cv2.imshow("bgr", image_bgr)
 
     ## set mouse click
     cv2.namedWindow('bgr')
     cv2.setMouseCallback('bgr', pick_color)
-
-    # now click into the hsv img , and look at values:
-    image_bgr = cv2.cvtColor(image_src, cv2.COLOR_BGR2RGB)
-    cv2.imshow("rgb", image_bgr)
 
     cv2.waitKey(50000)
     cv2.destroyAllWindows()
